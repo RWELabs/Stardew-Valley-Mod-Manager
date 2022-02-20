017b3afcf8e84685368ba83cbf9f907d0df144ee
Ryan Walpole Enterprises <35326939+RyanWalpoleEnterprises@users.noreply.github.com>
2022-02-21 01:57:21 +1100

    Upload Source Code
    Upload most recent copy of source code.

---
+++ source-code/Step2.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Stardew_Valley__The_Potato_Farm___Modded_Framework_
+{
+    partial class Step2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Step2));
+            this.Next = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.steamappscommon = new System.Windows.Forms.TextBox();
+            this.CopyBox = new System.Windows.Forms.RichTextBox();
+            this.validate = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Next
+            // 
+            this.Next.Location = new System.Drawing.Point(718, 280);
+            this.Next.Name = "Next";
+            this.Next.Size = new System.Drawing.Size(117, 23);
+            this.Next.TabIndex = 13;
+            this.Next.Text = "Next";
+            this.Next.UseVisualStyleBackColor = true;
+            this.Next.Click += new System.EventHandler(this.Next_Click);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(354, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(481, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Step One:";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(354, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(481, 188);
+            this.label2.TabIndex = 9;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(353, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(331, 21);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Where is your Stardew Valley game located?";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Stardew_Valley__The_Potato_Farm___Modded_Framework_.Properties.Resources.crutiofarmer_final;
+            this.pictureBox1.Location = new System.Drawing.Point(0, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(334, 334);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(354, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(481, 15);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Step Two:";
+            // 
+            // steamappscommon
+            // 
+            this.steamappscommon.Location = new System.Drawing.Point(357, 248);
+            this.steamappscommon.Name = "steamappscommon";
+            this.steamappscommon.Size = new System.Drawing.Size(478, 20);
+            this.steamappscommon.TabIndex = 15;
+            // 
+            // CopyBox
+            // 
+            this.CopyBox.Location = new System.Drawing.Point(12, 6);
+            this.CopyBox.Name = "CopyBox";
+            this.CopyBox.Size = new System.Drawing.Size(308, 96);
+            this.CopyBox.TabIndex = 16;
+            this.CopyBox.Text = "";
+            // 
+            // validate
+            // 
+            this.validate.Location = new System.Drawing.Point(12, 108);
+            this.validate.Name = "validate";
+            this.validate.Size = new System.Drawing.Size(308, 96);
+            this.validate.TabIndex = 17;
+            this.validate.Text = "";
+            // 
+            // Step2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(864, 328);
+            this.Controls.Add(this.steamappscommon);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Next);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.validate);
+            this.Controls.Add(this.CopyBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(880, 367);
+            this.MinimumSize = new System.Drawing.Size(880, 367);
+            this.Name = "Step2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Stardew Valley (The Potato Farm) [Modded Framework]";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Next;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox steamappscommon;
+        private System.Windows.Forms.RichTextBox CopyBox;
+        private System.Windows.Forms.RichTextBox validate;
+    }
+}
