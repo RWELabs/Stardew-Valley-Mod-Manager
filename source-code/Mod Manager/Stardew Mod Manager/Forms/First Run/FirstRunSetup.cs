d31c6759293163c9b51d7c31a8932f6d39a3c218
Ryan Walpole Enterprises <35326939+RyanWalpoleEnterprises@users.noreply.github.com>
2022-03-08 14:01:08 +1100

    Update FirstRunSetup.cs
    Unification of MessageBox p1
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

--- source-code/Mod Manager/Stardew Mod Manager/Forms/First Run/FirstRunSetup.cs
+++ source-code/Mod Manager/Stardew Mod Manager/Forms/First Run/FirstRunSetup.cs
@@ -28,7 +28,7 @@ namespace Stardew_Mod_Manager
                 if (Directory.GetFiles(AttemptedPath, "Stardew Valley.exe").Length == 0)
                 {
                     //No Stardew Valley Installation Found
-                    MessageBox.Show("We weren't able to find a valid Stardew Valley installation at that directory. Try watching the GIF again, making sure to follow the instructions closely - then try again. The path should include words like 'steamapps', 'common' and 'Stardew Valley'", "Stardew Valley Modded Framework", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("We weren't able to find a valid Stardew Valley installation at that directory. Try watching the GIF again, making sure to follow the instructions closely - then try again. The path should include words like 'steamapps', 'common' and 'Stardew Valley'", "Setup | Stardew Valley Modded Framework", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
                 else
                 {
@@ -50,7 +50,7 @@ namespace Stardew_Mod_Manager
                         //Show SMAPI Install Guide
                         //SDVDirPath.SelectAll();
                         SDVDirPath.Copy();
-                        MessageBox.Show("You don't seem to have SMAPI installed. We'll run you through the install process now. Once you have completed the installation of SMAPI, come back to this window and click 'Continue'. If SMAPI asks for your game path, we've copied it to your clipboard - but it's the same path you just entered.");
+                        MessageBox.Show("You don't seem to have SMAPI installed. We'll run you through the install process now. Once you have completed the installation of SMAPI, come back to this window and click 'Continue'. If SMAPI asks for your game path, we've copied it to your clipboard - but it's the same path you just entered.", "Setup | Stardew Valley Modded Framework", MessageBoxButtons.OK, MessageBoxIcon.Information);
                         try
                         {
                             string appPath = Path.GetDirectoryName(Application.ExecutablePath);
@@ -58,7 +58,7 @@ namespace Stardew_Mod_Manager
                         }
                         catch
                         {
-                            MessageBox.Show("SMAPI Installer was unable to launch.");
+                            MessageBox.Show("SMAPI Installer was unable to launch.", "Setup | Stardew Valley Modded Framework", MessageBoxButtons.OK, MessageBoxIcon.Error);
                         }
                     }
                 }
@@ -66,7 +66,7 @@ namespace Stardew_Mod_Manager
             catch
             {
                 //Path not valid.
-                MessageBox.Show("The text you entered doesn't seem to be a valid file path. Please re-read the instructions and try again.", "Stardew Valley Modded Framework", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("The text you entered doesn't seem to be a valid file path. Please re-read the instructions and try again.", "Setup | Stardew Valley Modded Framework", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
