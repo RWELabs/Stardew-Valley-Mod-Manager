e8b99eb91b769d5863035df6725d29c6a375baa5
Ryan Walpole <ryan.walpole@outlook.com>
2022-03-30 11:39:15 +1100

    Update MainPage.Designer.cs
    Edit MainPage to hide ModFolderPath and richTextBox1 as they are purely internal - were visible in #31 when window was resized
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

--- source-code/Mod Manager/Stardew Mod Manager/Forms/MainPage.Designer.cs
+++ source-code/Mod Manager/Stardew Mod Manager/Forms/MainPage.Designer.cs
@@ -162,6 +162,7 @@ namespace Stardew_Mod_Manager
             this.ModFolderPath.Name = "ModFolderPath";
             this.ModFolderPath.Size = new System.Drawing.Size(455, 20);
             this.ModFolderPath.TabIndex = 4;
+            this.ModFolderPath.Visible = false;
             // 
             // label1
             // 
@@ -201,6 +202,7 @@ namespace Stardew_Mod_Manager
             this.richTextBox1.Size = new System.Drawing.Size(85, 22);
             this.richTextBox1.TabIndex = 9;
             this.richTextBox1.Text = "";
+            this.richTextBox1.Visible = false;
             // 
             // LoadPreset
             // 
@@ -796,12 +798,12 @@ namespace Stardew_Mod_Manager
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(546, 581);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.SMAPIWarning);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.ModFolderPath);
             this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.ModFolderPath);
             this.Controls.Add(this.TabControl);
-            this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(562, 620);
