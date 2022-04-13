33d59b04dfb66d2182e42aea8143415a786adb09
Ryan Walpole Enterprises <ryan.walpole@outlook.com>
2022-04-13 15:10:36 +1000

    Version Update
    Update version number to v220402
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

--- source-code/Mod Manager/Stardew Mod Manager/Properties/Settings.Designer.cs
+++ source-code/Mod Manager/Stardew Mod Manager/Properties/Settings.Designer.cs
@@ -145,7 +145,7 @@ namespace Stardew_Mod_Manager.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("220401")]
+        [global::System.Configuration.DefaultSettingValueAttribute("220402")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
