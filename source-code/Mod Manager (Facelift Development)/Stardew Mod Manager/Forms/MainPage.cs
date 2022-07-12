cd31d1c0efc9e9385d1d847424bfc11346de1bc2
Ryan Walpole <ryan.walpole@outlook.com>
2022-07-13 08:33:53 +1000

    Update MainPage.cs
    Remove legacy form flickering code that was implemented briefly during the facelift update.
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

--- source-code/Mod Manager (Facelift Development)/Stardew Mod Manager/Forms/MainPage.cs
+++ source-code/Mod Manager (Facelift Development)/Stardew Mod Manager/Forms/MainPage.cs
@@ -103,26 +103,6 @@ namespace Stardew_Mod_Manager
             }
         }
 
-
-        /// <summary>
-        /// For avoid flickering Form
-        /// </summary>
-
-        internal static class NativeWinAPI
-        {
-
-            internal static readonly int GWL_EXSTYLE = -20;
-
-            internal static readonly int WS_EX_COMPOSITED = 0x02000000;
-
-            [DllImport("user32")]
-            internal static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-
-            [DllImport("user32")]
-            internal static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-
-        }
-
         private void CompareVersions()
         {
             string SMAPIVERNUM = SMAPIVer.Text;
