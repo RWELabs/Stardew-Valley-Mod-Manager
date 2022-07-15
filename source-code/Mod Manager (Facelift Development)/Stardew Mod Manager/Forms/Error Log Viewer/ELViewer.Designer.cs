f1787a0276f03f4bc1c68f0d32f7567b8a69512a
Ryan Walpole <ryan.walpole@outlook.com>
2022-07-15 15:07:03 +1000

    Implement #89 (ELViewer.cs)
    Create an error log viewer that loads an error log from launch arguments, so users can open the error log files to view the contents.
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

---
+++ source-code/Mod Manager (Facelift Development)/Stardew Mod Manager/Forms/Error Log Viewer/ELViewer.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Stardew_Mod_Manager.Forms.Error_Log_Viewer
+{
+    partial class ELViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ErrorLogContents = new System.Windows.Forms.RichTextBox();
+            this.SubmitLogs = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // ErrorLogContents
+            // 
+            this.ErrorLogContents.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ErrorLogContents.BackColor = System.Drawing.Color.Black;
+            this.ErrorLogContents.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ErrorLogContents.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ErrorLogContents.ForeColor = System.Drawing.Color.White;
+            this.ErrorLogContents.Location = new System.Drawing.Point(12, 38);
+            this.ErrorLogContents.Name = "ErrorLogContents";
+            this.ErrorLogContents.Size = new System.Drawing.Size(594, 203);
+            this.ErrorLogContents.TabIndex = 0;
+            this.ErrorLogContents.Text = "";
+            // 
+            // SubmitLogs
+            // 
+            this.SubmitLogs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SubmitLogs.AutoSize = true;
+            this.SubmitLogs.LinkColor = System.Drawing.SystemColors.AppWorkspace;
+            this.SubmitLogs.Location = new System.Drawing.Point(500, 12);
+            this.SubmitLogs.Name = "SubmitLogs";
+            this.SubmitLogs.Size = new System.Drawing.Size(106, 13);
+            this.SubmitLogs.TabIndex = 1;
+            this.SubmitLogs.TabStop = true;
+            this.SubmitLogs.Text = "Submit to RWE Labs";
+            this.SubmitLogs.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.SubmitLogs_LinkClicked);
+            // 
+            // ELViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(618, 253);
+            this.Controls.Add(this.SubmitLogs);
+            this.Controls.Add(this.ErrorLogContents);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "ELViewer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Error Log";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ELViewer_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox ErrorLogContents;
+        private System.Windows.Forms.LinkLabel SubmitLogs;
+    }
+}
---
+++ source-code/Mod Manager (Facelift Development)/Stardew Mod Manager/Forms/Error Log Viewer/ELViewer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stardew_Mod_Manager.Forms.Error_Log_Viewer
+{
+    public partial class ELViewer : Form
+    {
+        public ELViewer()
+        {
+            InitializeComponent();
+
+            ErrorLogContents.LoadFile(Properties.Settings.Default.LaunchArguments, RichTextBoxStreamType.PlainText);
+            Properties.Settings.Default.LaunchArguments = null;
+            Properties.Settings.Default.Save();
+        }
+
+        private void SubmitLogs_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://github.com/RWELabs/Stardew-Valley-Mod-Manager/issues/new?assignees=&labels=&template=issue-report.yaml&title=%5BIssue%5D+%20[ErrorLog]");
+        }
+
+        private void ELViewer_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
