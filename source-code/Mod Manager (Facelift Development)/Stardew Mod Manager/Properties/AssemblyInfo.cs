48e8ba3669499b80e10f3cdacdc6cafe8dffdbb2
Ryan Walpole <ryan.walpole@outlook.com>
2022-07-15 12:03:47 +1000

    Update Version Control (2)
    Update version number in the file assembly information.
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

--- source-code/Mod Manager (Facelift Development)/Stardew Mod Manager/Properties/AssemblyInfo.cs
+++ source-code/Mod Manager (Facelift Development)/Stardew Mod Manager/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("22.07.00.03")]
-[assembly: AssemblyFileVersion("22.07.00.03")]
+[assembly: AssemblyVersion("22.07.00.04")]
+[assembly: AssemblyFileVersion("22.07.00.04")]
 [assembly: NeutralResourcesLanguage("en-AU")]
