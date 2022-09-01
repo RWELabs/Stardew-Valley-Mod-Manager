f41a5c0812792a5025e6b8047920b5f163564605
Ryan Walpole <ryan.walpole@outlook.com>
2022-09-02 09:57:32 +1000

    Update Settings (Version Number)
    Update version number in application settings to 220902 (v220902)
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

--- source-code/Mod Manager (Facelift Development)/Stardew Mod Manager/Properties/Settings.Designer.cs
+++ source-code/Mod Manager (Facelift Development)/Stardew Mod Manager/Properties/Settings.Designer.cs
@@ -145,7 +145,7 @@ namespace Stardew_Mod_Manager.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("220901")]
+        [global::System.Configuration.DefaultSettingValueAttribute("220902")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
