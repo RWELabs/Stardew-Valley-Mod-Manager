4dc441cd6b137fc1cc0ec4f8e3e116c9b397f40d
Ryan Walpole <ryan.walpole@outlook.com>
2022-09-06 15:20:01 +1000

    Update Window - WebTools (Code)
    Create a WebView Dialog whenever the URL is not an RWE Labs URL for safety reasons. May implement this better in the future.
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

--- source-code/Mod Manager (Facelift Development)/Stardew Mod Manager/Forms/Webapp/WebToolsHome.cs
+++ source-code/Mod Manager (Facelift Development)/Stardew Mod Manager/Forms/Webapp/WebToolsHome.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,7 +18,8 @@ namespace Stardew_Mod_Manager.Forms.Webapp
         {
             if(!Directory.Exists(@"C:\Program Files (x86)\Microsoft\EdgeWebView\Application"))
             {
-
+                //Alert and Install Options
+                this.Close();
             }
             else
             {
@@ -29,6 +31,12 @@ namespace Stardew_Mod_Manager.Forms.Webapp
         {
             if (webView.CanGoBack) { Back.Enabled = true; } else if (!webView.CanGoBack) { Back.Enabled = false; }
             if (webView.CanGoForward) { Forward.Enabled = true; } else if (!webView.CanGoForward) { Forward.Enabled = false; }
+
+            string URL = webView.CoreWebView2.Source.ToString();
+            if (!URL.StartsWith("https://rwelabs.github.io/"))
+            {
+                webView.CoreWebView2.ExecuteScriptAsync($"alert('{URL} is not a Stardew Valley Mod Manager WebTools URL. This website and it's safety cannot be verified by RWE Labs. Proceed using this resource with caution.')");
+            };
         }
 
         private void Refresh_Click(object sender, EventArgs e)
