14c606aac98ed3ffc376a4e27c26f57ccffda84f
Ryan Walpole <ryan.walpole@outlook.com>
2022-09-06 13:08:29 +1000

    Create Window (WebTools)
    Create a new window called "WebTools" for accessing the webtools interface.
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

---
+++ source-code/Mod Manager (Facelift Development)/Stardew Mod Manager/Forms/Webapp/WebToolsHome.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stardew_Mod_Manager.Forms.Webapp
+{
+    public partial class WebToolsHome : Form
+    {
+        public WebToolsHome()
+        {
+            if(!Directory.Exists(@"C:\Program Files (x86)\Microsoft\EdgeWebView\Application"))
+            {
+
+            }
+            else
+            {
+                InitializeComponent();
+            }
+        }
+
+        private void webView_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+        {
+            if (webView.CanGoBack) { Back.Enabled = true; } else if (!webView.CanGoBack) { Back.Enabled = false; }
+            if (webView.CanGoForward) { Forward.Enabled = true; } else if (!webView.CanGoForward) { Forward.Enabled = false; }
+        }
+
+        private void Refresh_Click(object sender, EventArgs e)
+        {
+            webView.Reload();
+        }
+
+        private void Home_Click(object sender, EventArgs e)
+        {
+            webView.NavigateToString("https://rwelabs.github.io/sdvmm/webtools/");
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            webView.GoBack();
+        }
+
+        private void Forward_Click(object sender, EventArgs e)
+        {
+            webView.GoForward();
+        }
+    }
+}
