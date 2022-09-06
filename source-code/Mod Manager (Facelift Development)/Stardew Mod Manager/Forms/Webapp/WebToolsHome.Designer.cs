4a684093ce0a364559d6f0a5da199e94547a0537
Ryan Walpole <ryan.walpole@outlook.com>
2022-09-06 13:08:50 +1000

    Create Window (WebTools) (UI)
    Create the user interface for the WebTools window.
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

---
+++ source-code/Mod Manager (Facelift Development)/Stardew Mod Manager/Forms/Webapp/WebToolsHome.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Stardew_Mod_Manager.Forms.Webapp
+{
+    partial class WebToolsHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.Back = new System.Windows.Forms.ToolStripButton();
+            this.Home = new System.Windows.Forms.ToolStripButton();
+            this.Refresh = new System.Windows.Forms.ToolStripButton();
+            this.Forward = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Location = new System.Drawing.Point(0, 40);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(800, 582);
+            this.webView.Source = new System.Uri("https://rwelabs.github.io/sdvmm/webtools/", System.UriKind.Absolute);
+            this.webView.TabIndex = 0;
+            this.webView.ZoomFactor = 1D;
+            this.webView.NavigationCompleted += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs>(this.webView_NavigationCompleted);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.AutoSize = false;
+            this.toolStrip1.GripMargin = new System.Windows.Forms.Padding(30, 2, 2, 2);
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(25, 25);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Back,
+            this.Home,
+            this.Refresh,
+            this.Forward});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(10, 0, 1, 0);
+            this.toolStrip1.Size = new System.Drawing.Size(800, 41);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Back
+            // 
+            this.Back.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Back.Enabled = false;
+            this.Back.Image = global::Stardew_Mod_Manager.Properties.Resources.WTBack;
+            this.Back.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Back.Name = "Back";
+            this.Back.Padding = new System.Windows.Forms.Padding(0, 0, 20, 0);
+            this.Back.Size = new System.Drawing.Size(49, 38);
+            this.Back.Text = "Back";
+            this.Back.ToolTipText = "Back";
+            this.Back.Click += new System.EventHandler(this.Back_Click);
+            // 
+            // Home
+            // 
+            this.Home.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Home.Image = global::Stardew_Mod_Manager.Properties.Resources.WTHome;
+            this.Home.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Home.Name = "Home";
+            this.Home.Padding = new System.Windows.Forms.Padding(0, 0, 20, 0);
+            this.Home.Size = new System.Drawing.Size(49, 38);
+            this.Home.Text = "Home";
+            this.Home.ToolTipText = "Home";
+            this.Home.Click += new System.EventHandler(this.Home_Click);
+            // 
+            // Refresh
+            // 
+            this.Refresh.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Refresh.Image = global::Stardew_Mod_Manager.Properties.Resources.WTRefresh;
+            this.Refresh.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Refresh.Name = "Refresh";
+            this.Refresh.Padding = new System.Windows.Forms.Padding(0, 0, 20, 0);
+            this.Refresh.Size = new System.Drawing.Size(49, 38);
+            this.Refresh.Text = "Refresh";
+            this.Refresh.ToolTipText = "Refresh";
+            this.Refresh.Click += new System.EventHandler(this.Refresh_Click);
+            // 
+            // Forward
+            // 
+            this.Forward.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.Forward.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Forward.Enabled = false;
+            this.Forward.Image = global::Stardew_Mod_Manager.Properties.Resources.WTForward;
+            this.Forward.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Forward.Name = "Forward";
+            this.Forward.Padding = new System.Windows.Forms.Padding(0, 0, 20, 0);
+            this.Forward.Size = new System.Drawing.Size(49, 38);
+            this.Forward.Text = "Forward";
+            this.Forward.ToolTipText = "Forward";
+            this.Forward.Click += new System.EventHandler(this.Forward_Click);
+            // 
+            // WebToolsHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 622);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.webView);
+            this.Name = "WebToolsHome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WebToolsHome";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton Back;
+        private System.Windows.Forms.ToolStripButton Refresh;
+        private System.Windows.Forms.ToolStripButton Forward;
+        private System.Windows.Forms.ToolStripButton Home;
+    }
+}
