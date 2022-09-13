2644621aa98aa97e70b4effc2441c3a1755a6d73
Ryan Walpole <ryan.walpole@outlook.com>
2022-09-13 17:00:38 +1000

    Create Window (Telemetry Onboarding)
    Create the code behind the telemetry onboarding window, simple allow and deny buttons that change settings and then close the window.
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

---
+++ source-code/Mod Manager (Facelift Development)/Stardew Mod Manager/Forms/TelemetryOnboarding.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stardew_Mod_Manager.Forms
+{
+    public partial class TelemetryOnboarding : Form
+    {
+        public TelemetryOnboarding()
+        {
+            InitializeComponent();
+        }
+
+        private void AllowTelemetry_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.DoTelemetry = "TRUE";
+            Properties.Settings.Default.Save();
+
+            this.Close();
+        }
+
+        private void DenyTelemetry_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.DoTelemetry = "FALSE";
+            Properties.Settings.Default.Save();
+
+            this.Close();
+        }
+
+        private void ViewTelemetryPolicy_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://rwelabs.github.io/sdvmm/policies/#Telemetry");
+        }
+    }
+}
