3a2327c5a4867ef3815b9232bdfb63b1ed34eb7a
Ryan Walpole <ryan.walpole@outlook.com>
2022-09-16 14:16:14 +1000

    Create Repair Application Script
    Create a new script for repairing SDV Mod Manager
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

---
+++ source-code/Repair Utility/debug.bat
@@ -0,0 +1,46 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFAlDTQiSM2e/OpET/+b34OuDsXEZUeQ+bYPPybCYKeMc5AvtdplN
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJielUEFUrSbCXqVNU=
+::ZQ05rAF9IBncCkqN+0xwdVsGHkrSZSXqVvtMuog=
+::ZQ05rAF9IAHYFVzEqQICLQtVRRbCCX+zCrwI8aj59qq1lWhdeOM9a+8=
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQICLQtVRRbCKX+zCrwI8aj94PjHkVkcRuY6b8/t27iCJflTx0rgNbss2HtdndlMDgIYTCWOLiM1q30i
+::dhA7uBVwLU+EWGuz0QIcKRlHLA==
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATEphJielsZDDa1GSuWB7cPpKjJ9uuJ4nocWPIwdIqb/7qaJfID+Ez3cIVN
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFAlDTQiSM2e/OpET6/326uSTsXE6XfYXbY3n6aCPMuQW/QjSdJoh02MXtcQIQT1Kfx+ueQAmolJRuXGXNMLRsADlT3Sk6U9wNmR6hmTVnmtzRtJrn88K1TT+2EjpmqkfwXX6UL1HKVD1wL5sPd5A0AWwKGTJlLJST+TSabTnPwTNL2wen3X/mId8jIY9XilnAB1Cjtk=
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+echo Repair Utility - RWE Labs
+echo Stardew Valley Mod Manager
+echo (C) Copyright 2022 - RWE Labs, Ryan Walpole Enterprises
+echo Repair Utility v220902_bundled
+echo[
+echo[
+echo INFO:
+echo We're launching Stardew Valley Mod Manager in repair mode. Once the application has started, you may close this window or press any key to close this window.
+echo[
+echo[
+start "" "C:\Program Files (x86)\RWE Labs\Stardew Valley Mod Manager\Stardew Mod Manager.exe"  %repair
+pause
